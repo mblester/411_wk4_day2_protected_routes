54ff5fd850d257c9b43e257dd8ba590ed3683463
mblester <morganmurry@gmail.com>
2022-09-06 18:17:35 -0500

    finished login

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,6 +23,7 @@ const Login = () => {
   const login = (e) => {
     e.preventDefault();
     // set cookie here
+    document.cookie = "loggedIn=true;max-age=60";
     // set loggedIn = true and max-age = 60*1000 (one minute)
 
     navigate("/");
